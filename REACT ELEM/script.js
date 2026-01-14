647bc42f736442eb637d20fa17b830b321425ec0
frager_098 <hafizmuhammadarham042@gmail.com>
2026-01-14 15:03:39 -0800

    Render in container div

--- REACT ELEM/script.js
+++ REACT ELEM/script.js
@@ -1,16 +1,33 @@
-const firstBasket = React.createElement("div",{ key: "basket1"},
+const container = React.createElement('div',{className:"row container", key:"container"},[
+    React.createElement("div",{key:"one"},
+        [
+        React.createElement("h2",{key:"heading__1"},"Basket 1"),
+        React.createElement("p",{key:"paragraph__1"},"10 Apples"),
+        React.createElement("button",{key:"button__1"},"->")
+        ]
+    ),
+    React.createElement("div",{key:"two"},
     [
-    React.createElement("h2",{key:'1'},"Basket 1"),
-    React.createElement("p",{key:'2'},"10 Apples"),
-    React.createElement("button",{key:'3',id:'basket__button--1'},"->"),
+    React.createElement("h2",{key:'heading__2'},"Basket 2"),
+    React.createElement("p",{key:'paragraph__2'},"0 Apples"),
+    React.createElement("button",{key:'button__2',id:'basket__button--2'},"<-"),
     ]
-);
-const secondBasket = React.createElement("div",{ key: "basket2" },
-    [
-    React.createElement("h2",{key:'1'},"Basket 2"),
-    React.createElement("p",{key:'2'},"0 Apples"),
-    React.createElement("button",{key:'3',id:'basket__button--2'},"<-"),
+    )
     ]
-);
+)
+// const firstBasket = React.createElement("div",{ key: "basket1"},
+//     [
+//     React.createElement("h2",{key:'1'},"Basket 1"),
+//     React.createElement("p",{key:'2'},"10 Apples"),
+//     React.createElement("button",{key:'3',id:'basket__button--1'},"->"),
+//     ]
+// );
+// const secondBasket = React.createElement("div",{ key: "basket2" },
+//     [
+//     React.createElement("h2",{key:'1'},"Basket 2"),
+//     React.createElement("p",{key:'2'},"0 Apples"),
+//     React.createElement("button",{key:'3',id:'basket__button--2'},"<-"),
+//     ]
+// );
 const root = ReactDOM.createRoot(document.querySelector('.root'))
-root.render([firstBasket,secondBasket]);
+root.render(container);
