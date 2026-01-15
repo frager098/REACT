0a493d8f23096b4922d8b0421882070a5c20f74a
frager_098 <hafizmuhammadarham042@gmail.com>
2026-01-14 16:22:56 -0800

    Add images and form

--- REACT ELEM/script.js
+++ REACT ELEM/script.js
@@ -1,4 +1,5 @@
-const container = React.createElement('div',{className:"row container", key:"container"},[
+const container = React.createElement('div',{className:"row container", key:"container"},
+    [
     React.createElement("div",{key:"one"},
         [
         React.createElement("h2",{key:"heading__1"},"Basket 1"),
@@ -7,12 +8,33 @@ const container = React.createElement('div',{className:"row container", key:"con
         ]
     ),
     React.createElement("div",{key:"two"},
-    [
-    React.createElement("h2",{key:'heading__2'},"Basket 2"),
-    React.createElement("p",{key:'paragraph__2'},"0 Apples"),
-    React.createElement("button",{key:'button__2',id:'basket__button--2'},"<-"),
-    ]
+        [
+            React.createElement("h2",{key:'heading__2'},"Basket 2"),
+            React.createElement("p",{key:'paragraph__2'},"0 Apples"),
+            React.createElement("button",{key:'button__2',id:'basket__button--2'},"<-"),
+        ]),
+    React.createElement("div",{key:"three", className:"react__logo"},
+        React.createElement('img',{key:"k", src:"./Images/React-icon.svg.png",
+            alt:"React-logo"
+        })
+    ),
+    React.createElement("div",{key:"four"},
+       React.createElement('form',{action:"#"},
+        "Name ",
+        React.createElement('input',{type:"text",placeholder:"Enter Name..."},)
+       )
     )
+    //,React.createElement("div",{key:"five"},
+    //     [
+    //         React.createElement("h2",{key:'heading__2'},"Basket 2"),
+    //         React.createElement("p",{key:'paragraph__2'},"0 Apples"),
+    //         React.createElement("button",{key:'button__2',id:'basket__button--2'},"<-"),
+    //     ]),
+    // React.createElement("div",{key:"six", className:"react__logo"},
+    //     React.createElement('img',{key:"k", src:"./Images/React-icon.svg.png",
+    //         alt:"React-logo"
+    //     })
+    // )
     ]
 )
 // const firstBasket = React.createElement("div",{ key: "basket1"},
