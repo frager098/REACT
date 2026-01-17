efeb235684b00254e574232ff26a16c7644881ea
frager_098 <hafizmuhammadarham042@gmail.com>
2026-01-16 17:37:35 -0800

    Get start with Babel using CDN

---
+++ JSX/script.js
@@ -0,0 +1,12 @@
+// const h2 = <h2>Hello</h2>
+// const h2 = "Hello"
+    const arrowFunc = () => "Hi"
+const container = <div>
+    <h2>Container</h2>
+    <p>That's how we write html like code in JS through Bable Toolchain</p>
+    <p>{arrowFunc()}</p>
+</div>
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(container)
