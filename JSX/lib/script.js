3f82c5f25769559b5e68b50ecb4af4210231c680
frager_098 <hafizmuhammadarham042@gmail.com>
2026-01-18 07:09:44 +0500

    Generate Source Map

--- JSX/lib/script.js
+++ JSX/lib/script.js
@@ -1,9 +1,8 @@
 // const h2 = <h2>Hello</h2>
 // const h2 = "Hello"
 const arrowFunc = () => "Hi";
-const container = /*#__PURE__*/React.createElement("div", null, 
-    /*#__PURE__*/React.createElement("h2", null, "Container"), 
-    /*#__PURE__*/React.createElement("p", null, "That's how we write html like code in JS through Bable Toolchain"), 
-    /*#__PURE__*/React.createElement("p", null, arrowFunc()));
+const container = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "Container"), /*#__PURE__*/React.createElement("p", null, "That's how we write html like code in JS through Bable Toolchain"), /*#__PURE__*/React.createElement("p", null, arrowFunc()));
+console.log(container);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(container);
+//# sourceMappingURL=script.js.map
--- JSX/script.js
+++ JSX/script.js
@@ -1,13 +1,13 @@
 // const h2 = <h2>Hello</h2>
 // const h2 = "Hello"
-    const arrowFunc = () => "Hi"
+const arrowFunc = () => "Hi"
 const container = <div>
     <h2>Container</h2>
     <p>That's how we write html like code in JS through Bable Toolchain</p>
     <p>{arrowFunc()}</p>
 </div>
 
-
+console.log(container);
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(container)
